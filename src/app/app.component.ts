b17f0687540f15993cf970d34e90863060281af8
Anton Sobolevskiy <pipkin.aanton@gmail.com>
2017-02-23 12:37:34 +0200

    start learn unit test

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+/*
+ * App Component
+ * Top Level Component
+ */
+@Component({
+  selector: 'app',
+  template: ``
+})
+export class AppComponent {}
---
+++ src/app/app.routes.ts
@@ -0,0 +1,5 @@
+import { Routes } from '@angular/router';
+
+import { DataResolver } from './app.resolver';
+
+export const ROUTES: Routes = [];
