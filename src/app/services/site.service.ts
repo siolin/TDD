85619a63932446a9ff01324d9a8ff475aa8b19e3
Nick <nickgulya@gmail.com>
2017-03-09 10:54:40 +0200

    Added tests for site service

--- src/app/services/site.service.ts
+++ src/app/services/site.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Injectable()
-export class SiteService {}
+export class SiteService {
+
+  private domain: string = `https://randomuser.me/ap1/`;
+
+  constructor(
+    private http: Http
+  ) {}
+
+  public getUsers() {
+    return this.http.get(`${this.domain}?result=10`)
+      .map((data) => data.json());
+  }
+
+}
